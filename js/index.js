2f1d0f8471a3b9624282735aed5054aa37ab6c0b
nurrakhman <nurrakhman.gincode@gmail.com>
2022-01-27 13:57:14 +0700

    boilerplate

---
+++ js/index.js
@@ -0,0 +1,7 @@
+import akg from "akg";
+import bloodPressure from "bloodPressure";
+import bloodSugar from "bloodSugar";
+import bmi from "bmi";
+import cholesterol from "cholesterol";
+
+console.log(akg());
