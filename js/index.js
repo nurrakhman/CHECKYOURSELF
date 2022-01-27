5ba521be4d25d396b030ac5cb10a805fb71b0731
rosaalwyh <alawiyahrosa@gmail.com>
2022-01-27 15:51:50 +0700

    done

--- js/index.js
+++ js/index.js
@@ -1,15 +1,12 @@
 
 const bloodPressure = require('./bloodPressure');
 const bloodSugar = require('./bloodSugar');
-const bloodPressure = require('./bloodPressure')
-const akg = require('./akg')
+const akg = require('./akg');
 
 // import bloodPressure from "bloodPressure";
 // import bloodSugar from "bloodSugar";
 // import bmi from "bmi";
 // import cholesterol from "cholesterol";
-
-console.log(bloodPressure.count(90,70));
 console.log(bloodSugar.calculateBloogSugar(18,110));
 console.log(bloodPressure.count(90,70));
 console.log(akg.value(false, 75, 168, 28, 2));
